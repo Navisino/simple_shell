8b64d74995062296b47581e99c436dbbc4bec847
Navisino <navisinokate@gmail.com>
2022-09-07 09:57:16 -0700

    Shell

--- main.h
+++ main.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h> 
 #include <stdlib.h> 
+#include <errno.h>
 #include <unistd.h> 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -24,5 +25,45 @@ void prompt(void);
 void handle(int signals);
 void _EOF(char *buffer);
 void shell_exit(char **command);
+
+char *prompt_n_read(char *path_str, char **array_path);
+char **split_buf(char *buf);
+int token_count_buf(char *buf);
+int fork_n_exec(char *str_buf_path, char **array_buf);
+char *_getenv(const char *name);
+char **split_path(char *path_str);
+int token_count_path(char *path_str);
+int _strlen(char *s);
+char *buf_path_cat(int counter, char *av, char *array_buf, char **array_path);
+int _status(char *str_buf_path);
+char *_strcat(char *dest, char *src);
+char *_memset(char *s, char b, unsigned int n);
+char *_strdup(char *str);
+int _strcmp(char *s1, char *s2);
+int _putchar(char c);
+int _print(char *str);
+void print_number(int n);
+void sig_handler(int signo);
+
+int handle_cmd(char **av, int counter, char **array_buf, char **array_path,
+char *str_buf_path);
+int handle_path_cmd(char **av, int counter, char **array_buf);
+int handle_nopath_cmd(char **array_path, char **av, char **array_buf,
+int counter, char *str_buf_path);
+
+
+typedef struct builtins
+{
+	char *name;
+	void (*f)(char **, char *, char *, char **, int);
+} built_ins;
+
+void (*get_builtins_func(char *s))(char **, char *, char *, char **, int);
+void env_(char **array_buf, char *buf, char *path_str, char **array_path,
+int exit_status);
+void exit_(char **array_buf, char *buf, char *path_str, char **array_path,
+int exit_status);
+void help_(char **array_buf, char *buf, char *path_str, char **array_path,
+int exit_status);
 
 #endif
---
+++ process.c
@@ -0,0 +1,77 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <signal.h>
+
+char *prompt_n_read(char *path_str, char **array_path)
+{
+	char *buf = NULL;
+	size_t bufsize = 0;
+	int i = 0, status = 0;
+
+	if (isatty(STDIN_FILENO) == 1)
+		_print("$");
+	status = getline(&buf, &bufsize, stdin);
+	if (status == -1)
+	{
+		ree(buf);
+		free(array_path);
+		free(path_str);
+		if (isatty(STDIN_FILENO) == 1)
+			_print("\n");
+		exit(0);
+	}
+	for (i = 0; buf[i] != '\0'; i++)
+	{
+		if (buf[0] == '#' || (buf[i] == ' ' && buf[i + 1] == '#'))
+		{
+			buf[i] = '\0';
+			break;
+		}
+	}
+	return (buf);
+}
+
+
+char **split_buf(char *buf)
+{
+	int i = 0, count = 0;
+	char *token = NULL;
+	char **array_buf = NULL;
+
+	count = token_count_buf(buf);
+	if (count == 0)
+		return (NULL);
+	array_buf = malloc(sizeof(char *) * (count + 1));
+	if (array_buf == NULL)
+		exit(1);
+	token = strtok(buf, " \n\t");
+	while (token != NULL)
+	{
+		array_buf[i] = token;
+		i++;
+		token = strtok(NULL, " \n\t");
+	}
+	array_buf[i] = NULL;
+	return (array_buf);
+}
+
+int token_count_buf(char *buf)
+{
+	int i = 0, count = 0;
+
+	while (*buf != '\0')
+	{
+		if (*buf == ' ' || *buf == '\t')
+			count++;
+		i++, buf++;
+	}
+	count++;
+	return (count);
+}
