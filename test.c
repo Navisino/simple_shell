781a73eff380cfd63176603726c20880ea26cb0e
Navisino <navisinokate@gmail.com>
2022-09-07 04:06:04 -0700

    Test file

---
+++ test.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * This is a test file 
+ */
+
+int main(void)
+{
+	int x = 7;
+	int y = 3;
+	
+	if(x > y)
+	{
+		printf("x is greater than y.");
+	}
+	else 
+	{
+		printf("y is greater than x.");
+	}
+
+	return(0);
+}	
